a80679db74149e45869deb7b13a00bb7482e6a76
Andrew Sun <andrew@crossbowffs.com>
2025-09-14 21:10:06 -0700

    Allow reinstalling systemd units even if profile dir already exists

--- install.sh
+++ install.sh
@@ -18,27 +18,30 @@ install -Dm644 duplicity-backup@.timer /etc/systemd/system/
 install -Dm644 duplicity-backup-watchdog@.service /etc/systemd/user/
 install -Dm644 duplicity-backup-watchdog@.timer /etc/systemd/user/
 systemctl daemon-reload
-echo "installed duplicity-backup"
+echo "installed duplicity-backup binary and systemd units"
 
 if [ "$#" -eq 1 ]; then
     profile="$1"
     profile_dir="/etc/duplicity/${profile}"
 
-    if [ -e "${profile_dir}" ]; then
-        echo >&2 "${profile_dir} already exists, refusing to overwrite"
-        exit 1
+    if [ -d "${profile_dir}" ]; then
+        echo >&2 "${profile_dir} already exists, preserving existing configuration!"
+        echo >&2 "hint: delete ${profile_dir} and re-run this script to replace configuration"
+    else
+        if ! [ -f "conf.${profile}.example" ]; then
+            echo >&2 "create and edit conf.${profile}.example first"
+            exit 1
+        fi
+
+        install -m700 -d "${profile_dir}"
+        install -Dm600 "conf.${profile}.example" "${profile_dir}/conf"
+        install -Dm600 <(env -i "USER=${SUDO_USER}" envsubst < include.example) "${profile_dir}/include"
+        [ -f ssh_id.example ] && install -Dm600 ssh_id.example "${profile_dir}/ssh_id"
+        echo "installed configuration files to ${profile_dir}"
     fi
 
-    if ! [ -f "conf.${profile}.example" ]; then
-        echo >&2 "create and edit conf.${profile}.example first"
-        exit 1
-    fi
-
-    install -m700 -d "${profile_dir}"
-    install -Dm600 "conf.${profile}.example" "${profile_dir}/conf"
-    install -Dm600 <(env -i "USER=${SUDO_USER}" envsubst < include.example) "${profile_dir}/include"
-    [ -f ssh_id.example ] && install -Dm600 ssh_id.example "${profile_dir}/ssh_id"
     systemctl enable --now "duplicity-backup@${profile}.timer"
+    echo "enabled duplicity-backup@${profile}.timer"
     systemctl --user --machine ${SUDO_USER}@.host enable --now "duplicity-backup-watchdog@${profile}.timer"
-    echo "enabled duplicity-backup@${profile}"
+    echo "enabled duplicity-backup-watchdog@${profile}.timer"
 fi
